415383aadcc5f4c74f3eccb0b5f88c366c4c338c
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-02 14:38:39 +0900

    implement commenting on PR script

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import { getOctokit, context } from '@actions/github'
 
 /**
  * The main function for the action.
@@ -7,18 +7,24 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const pullRequest = context.payload.pull_request
+    if (!pullRequest) {
+      core.setFailed('No pull request found.')
+      return
+    }
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const token = core.getInput('github_token', { required: true })
+    const octokit = getOctokit(token)
+    const owner = context.repo.owner
+    const repo = context.repo.repo
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pullRequest.number,
+      body: 'Hello, this is my action!'
+    })
+    core.debug(`Commented on PR #${pullRequest.number}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
