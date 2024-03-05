408beafc09c93e1a59e57db0fa9306dad9d8f490
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-05 23:52:13 +0900

    Refactor options (#14)
    * extract options
    
    * fix input to pass pr_url instead of calculated pr_number
    This change makes workflow scripts simpler
    
    * apply this change
    
    * add type annotation

---
+++ src/option.ts
@@ -0,0 +1,31 @@
+import { getInput, setFailed } from '@actions/core'
+import { context } from '@actions/github'
+
+type Option = {
+  token: string
+  prNumber: number
+}
+
+function getPrNumber(): number {
+  if (typeof context.payload.pull_request?.number === 'number') {
+    return context.payload.pull_request.number
+  }
+
+  const url = getInput('pr_url', { required: false })
+  const numberFromUrl = Number(url.substring(url.lastIndexOf('/') + 1))
+  return numberFromUrl
+}
+
+export function getOption(): Option {
+  const token = getInput('github_token', { required: true })
+
+  const prNumber = getPrNumber()
+  if (isNaN(prNumber) || prNumber === 0) {
+    setFailed('pr number is not set properly')
+  }
+
+  return {
+    token,
+    prNumber
+  }
+}
