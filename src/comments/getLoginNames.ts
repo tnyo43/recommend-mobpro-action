fc2ec01566bd42e2521a1cbc2d405075ac5f9780
Tomoya Kashifuku <tnyo43@gmail.com>
2024-03-08 00:03:08 +0900

    create get login names function

---
+++ src/comments/getLoginNames.ts
@@ -0,0 +1,24 @@
+import { type User } from './types'
+
+function uniqueStringArray(texts: string[]): string[] {
+  if (texts.length === 0) return []
+
+  const sorted = texts.sort()
+  const result = [sorted[0]]
+
+  for (let i = 0; i < sorted.length - 1; i++) {
+    if (sorted[i + 1] !== sorted[i]) {
+      result.push(sorted[i + 1])
+    }
+  }
+
+  return result
+}
+
+export function getLoginNames(users: User[]): string[] {
+  const loginNameArray = users
+    .filter(user => user.type === 'User')
+    .map(user => user.login)
+
+  return uniqueStringArray(loginNameArray)
+}
---
+++ src/comments/types.ts
@@ -0,0 +1,7 @@
+export type User = {
+  // login id of user: ex. tnyo43
+  login: string
+
+  // type of user: "User" or "Bot"
+  type: string
+}
